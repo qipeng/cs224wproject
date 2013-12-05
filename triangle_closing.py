a9a338e79c6b178d0394ae2bcb6b1d09c7a39356
JiajiHu <jiajihu@stanford.edu>
2013-12-05 11:33:42 -0800

    Add function for triangle closing eval

---
+++ triangle_closing.py
@@ -0,0 +1,6 @@
+from snap import *
+
+# suppose we have done subtraction between two close snapshots
+# G2 is the later snapshot
+# E is the set of edges that are in G2 but not in G1
+# This function evaluates the triangle closing model on G2 and E
